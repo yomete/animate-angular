bd3e5f95791665fda87ebebf45e307afccf51dae
Yomi <yomi.eluwande@gmail.com>
2018-10-14 05:50:13 +0100

    animate states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations:[
+    trigger('move', [
+        state('forward', style({
+            'background-color':'green',
+            'transform':'translateX(100px)'
+        })),
+        state('backward', style({
+          'background-color':'blue',
+          'transform':'translateX(0px)'
+        })),
+        transition('* => *', animate(500))
+    ])
+  ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'animate-angular';
+  move; 
+
+  moveForward(){
+      this.move = 'forward';
+  }
+
+  moveBackward(){
+      this.move = 'backward';
+  }
 }
